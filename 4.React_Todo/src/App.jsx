9bd4518dae6b08826dbe98fdc59eef4922117063
subha20032007 <meunknown466@gmail.com>
2024-11-29 23:55:13 +0530

    Ad

--- 4.React_Todo/src/App.jsx
+++ 4.React_Todo/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  //123456789
+ const ref=useRef()
+
+console.log(ref)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hii From Happy</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input type="text"  ref={ref} />
+    <button onClick={()=>{
+      ref.current.focus()
+      console.log(ref.current.value)
+    }}>focus</button>
     </>
   )
 }
